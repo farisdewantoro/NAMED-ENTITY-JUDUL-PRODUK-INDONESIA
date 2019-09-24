4cbecaa8cddb08945e2b70f77c2257fa886f2409
Faris Dewantoro <farisd117@gmail.com>
2019-09-24 17:46:38 +0700

    add chrome-path

--- server/bot/Lazada.py
+++ server/bot/Lazada.py
@@ -183,7 +183,15 @@ class LazadaBot():
            
 
     def run_finding(self):
-        bot = webdriver.Firefox()
+        GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
+        CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.binary_location = GOOGLE_CHROME_PATH
+        # bot = webdriver.Firefox()
+        bot = webdriver.Chrome(
+            execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
         bot.set_window_position(0, 0)
         bot.set_window_size(320, 480)
         bot.get(
